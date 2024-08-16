0c4b4456259f05704b62f8aa2741f439311c2f42
Mohammad Seyed <mohammad.seyed.xv@renesas.com>
2024-08-16 23:58:42 +0200

    1

---
+++ AAC2M3H1.v
@@ -0,0 +1,10 @@
+module FullAdd4(A, B, Cin, Sum, Cout);
+
+    input [3:0] A, B; 
+    input Cin; 			 
+    output [3:0] Sum; 
+    output Cout; 
+
+    assign {Cout, Sum} = A + B + Cin; 
+
+endmodule
